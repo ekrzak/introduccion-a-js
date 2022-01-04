35a6945d554fd0d1bd44156a1e7062f01cfb1477
estebankrzak <50186552+estebankrzak@users.noreply.github.com>
2022-01-04 18:35:46 -0300

    Completa la parte1 de tarea-clase-6

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -6,6 +6,115 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
+const $cantidadIntegrantes = document.querySelector('#cantidad-integrantes');
+const $listaIntegrantes = document.querySelector('#lista-integrantes');
+
+function crearInputsPorIntegrante(cantidadIntegrantes) {
+    
+    for (let i = 0; i < cantidadIntegrantes; i++) {
+        const nuevaEtiqueta = document.createElement('label');
+        const textoEtiqueta = document.createTextNode(`Persona ${i+1}`);
+        const saltoDeLinea = document.createElement('br');
+        nuevaEtiqueta.appendChild(textoEtiqueta);
+        nuevaEtiqueta.htmlFor = `edad-persona-${i+1}`;
+        const nuevoInput = document.createElement('input');
+        nuevoInput.id = `edad-persona-${i+1}`;
+        nuevoInput.type = 'number';
+        nuevoInput.className = 'edades-personas';
+        nuevoInput.placeholder = 'Edad en años';
+        $listaIntegrantes.appendChild(nuevaEtiqueta);
+        $listaIntegrantes.appendChild(nuevoInput);
+        $listaIntegrantes.appendChild(saltoDeLinea);
+    }
+
+    
+}
+
+function crearBotonDeCalculo() {
+    const nuevoBoton = document.createElement('button'),
+        botonLimpiar = document.createElement('button');
+    nuevoBoton.type = 'submit';
+    nuevoBoton.textContent = 'Calcular!';
+    nuevoBoton.id = 'boton-calcular';
+    botonLimpiar.type = 'submit';
+    botonLimpiar.textContent = 'Limpiar';
+    botonLimpiar.id = 'boton-limpiar';
+    $listaIntegrantes.appendChild(nuevoBoton);
+    $listaIntegrantes.appendChild(botonLimpiar);
+}
+
+function obtenerElMenor(listaDeNodos) {
+    let esElMenor = listaDeNodos[0].value;
+    for (let i = 1; i < listaDeNodos.length; i++) {
+        if (esElMenor < listaDeNodos[i].value) {
+            continue;
+        } else {
+            esElMenor = listaDeNodos[i].value;
+        }
+    }
+    return Number(esElMenor);
+}
+
+function obtenerElMayor(listaDeNodos) {
+    let esElMayor = listaDeNodos[0].value;
+    for (let i = 1; i < listaDeNodos.length; i++) {
+        if (esElMayor > listaDeNodos[i].value) {
+            continue;
+        } else {
+            esElMayor = listaDeNodos[i].value;
+        }
+    }
+    return Number(esElMayor);
+}
+
+function obtenerElPromedio(listaDeNodos) {
+    let suma = 0;
+    for (let i = 0; i < listaDeNodos.length; i++) {
+        suma += Number(listaDeNodos[i].value);
+    }
+    return (suma / listaDeNodos.length);
+}
+
+document.querySelector('#ingresar').onclick = () => {
+    const cantidadDeIntegrantes = $cantidadIntegrantes.value;
+    // Crea los inputs para ingresar las edades
+    crearInputsPorIntegrante(cantidadDeIntegrantes);
+    document.querySelector('#ingresar').disabled = true;
+    // Genera el boton de calcular y de limpiar
+    crearBotonDeCalculo();
+    // Ejecuta las acciones sobre el boton de calcular
+    document.querySelector('#boton-calcular').onclick = () => {
+        const listaDeEdades = document.querySelectorAll('.edades-personas');        
+        document.querySelector('#mayor-edad').textContent = `La persona de mayor edad tiene ${obtenerElMayor(listaDeEdades)} años.`;
+        document.querySelector('#menor-edad').textContent = `La persona de menor edad tiene ${obtenerElMenor(listaDeEdades)} años.`;
+        document.querySelector('#promedio-edad').textContent = `El promedio de edad de las personas listadas es de ${obtenerElPromedio(listaDeEdades)} años`;
+        return false;
+    }
+    // Ejecuta las acciones sobre el boton de limpiar: elimina los inputs y los resultados calculados. Vuelve a activar el boton#ingresar
+    document.querySelector('#boton-limpiar').onclick = () => {
+        while ($listaIntegrantes.firstChild) { // Elimina todo lo del nodo padre form $listaIntegrantes
+            $listaIntegrantes.removeChild($listaIntegrantes.firstChild);
+          }
+        // Resetea los resultados de los textos
+        document.querySelector('#mayor-edad').textContent = "";
+        document.querySelector('#menor-edad').textContent = "";
+        document.querySelector('#promedio-edad').textContent = "";
+        // Vuelve a activar el boton#ingresar
+        document.querySelector('#ingresar').disabled = false;
+        // Vuelve el input al estado original
+        $cantidadIntegrantes.value = null;
+
+        return false;
+    }
+
+
+    return false;
+}
+
+
+
+
+
 
 /*
 TAREA:
