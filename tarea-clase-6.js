5417b5526d3ebf67bd7337df8c4e61935db0b0c8
estebankrzak <50186552+estebankrzak@users.noreply.github.com>
2022-01-07 18:49:30 -0300

    Completa  la tarea-clase-6

--- tarea-clase-6.js
+++ tarea-clase-6.js
@@ -43,6 +43,38 @@ function crearBotonDeCalculo() {
     $listaIntegrantes.appendChild(botonLimpiar);
 }
 
+const crearBotonesAgregarQuitarSueldos = function() { // Será una función que se ejecute una sola vez
+    let ejecutado = false; // variable a cambiar una vez que se ejecute
+    return function() {
+        if (!ejecutado) {
+            ejecutado = true; // acá la cambio
+            // Acá la función en sí: crea los botones de agregar y quitar y les pone id's.
+            const botonAgregar = document.createElement('button'),
+            botonQuitar = document.createElement('button');
+            botonAgregar.type = 'submit';
+            botonAgregar.textContent = 'Agregar sueldos';
+            botonAgregar.id = 'boton-agregar-sueldos';
+            botonQuitar.type = 'submit';
+            botonQuitar.textContent = 'Quitar sueldos';
+            botonQuitar.id = 'boton-quitar-sueldos';
+            $listaIntegrantes.appendChild(botonAgregar);
+            $listaIntegrantes.appendChild(botonQuitar);
+        }
+    }
+}();   
+
+function crearInputsDeSueldos(cantidadIntegrantes) {
+    for (let i = 0; i < cantidadIntegrantes; i++) {
+        
+        //agrega input
+        const nuevoInput = `<input type="number" id="sueldo-persona-${i+1}" class="sueldos-personas" placeholder="Sueldo anual en pesos"></input>`;
+        document.querySelectorAll('.edades-personas')[i].insertAdjacentHTML('afterend', nuevoInput);
+        // Agrega etiqueta
+        const nuevaEtiqueta = `<label for="sueldo-persona-${i+1}" class="etiquetas-sueldos">Sueldo $</label>`;
+        document.querySelector(`#sueldo-persona-${i+1}`).insertAdjacentHTML('beforebegin', nuevaEtiqueta);        
+    }
+}
+
 function obtenerElMenor(arreglo) {
     let esElMenor = arreglo[0];
     for (let i = 0; i < arreglo.length; i++) {
@@ -78,12 +110,35 @@ document.querySelector('#ingresar').onclick = () => {
     document.querySelector('#ingresar').disabled = true;
     // Genera el boton de calcular y de limpiar
     crearBotonDeCalculo();
+    // Genera los botones de agregar y quitar sueldos ------------------------------ se ejecuta una sola vez
+    crearBotonesAgregarQuitarSueldos();
+
+    // Ejecuta las acciones sobre el boton de agregar sueldos
+    document.querySelector('#boton-agregar-sueldos').onclick = function() {
+        crearInputsDeSueldos(Number(cantidadDeIntegrantes));
+        document.querySelector('#boton-agregar-sueldos').disabled = true; // Desactiva el botón para evitar múltiples columna
+
+        return false;
+    }
+
+    // Ejecuta acciones sobre el botón de quitar sueldos
+    document.querySelector('#boton-quitar-sueldos').onclick = function() {
+        
+        if (document.querySelector('#boton-agregar-sueldos').disabled === true) {
+
+            document.querySelectorAll('.sueldos-personas').forEach(e => e.remove()); // elimina los inputs de sueldos
+            document.querySelectorAll('.etiquetas-sueldos').forEach(e => e.remove()); // elimina los labels de sueldos
+            document.querySelector('#boton-agregar-sueldos').disabled = false; // resetea el botón de sueldos a nuevo
+           
+        }
+        return false;
+    }
+
     // Ejecuta las acciones sobre el boton de calcular
     document.querySelector('#boton-calcular').onclick = () => {
         // Pasa a array los values de la NodeList de los inputs
         const listaDeEdadesListaDeNodos = document.querySelectorAll('.edades-personas');
         const listaDeEdades = [];
-        console.log(listaDeEdadesListaDeNodos);
         for (i = 0; i < listaDeEdadesListaDeNodos.length; i++) {
             if (Number(listaDeEdadesListaDeNodos[i].value) >= 0) {
                 listaDeEdades.push(Number(listaDeEdadesListaDeNodos[i].value));
@@ -98,7 +153,30 @@ document.querySelector('#ingresar').onclick = () => {
         // Expresa los resultados
         document.querySelector('#mayor-edad').textContent = `La persona de mayor edad tiene ${obtenerElMayor(listaDeEdades)} años.`;
         document.querySelector('#menor-edad').textContent = `La persona de menor edad tiene ${obtenerElMenor(listaDeEdades)} años.`;
-        document.querySelector('#promedio-edad').textContent = `El promedio de edad de las personas listadas es de ${obtenerElPromedio(listaDeEdades)} años`;
+        document.querySelector('#promedio-edad').textContent = `El promedio de edad de las personas listadas es de ${obtenerElPromedio(listaDeEdades)} años.`;
+
+        // Pasa a array los valores de la NodeList de sueldos
+        const listaDeSueldosListaDeNodos = document.querySelectorAll('.sueldos-personas');
+        const listaDeSueldos = [];
+        for (i = 0; i < listaDeSueldosListaDeNodos.length; i++) {
+            if (listaDeSueldosListaDeNodos[i].value === null || Number(listaDeSueldosListaDeNodos[i].value) < 0) {
+                listaDeSueldosListaDeNodos[i].disabled = true;
+                listaDeSueldosListaDeNodos[i].style.backgroundColor = 'pink';
+            } else if (listaDeSueldosListaDeNodos[i].value !== null && Number(listaDeSueldosListaDeNodos[i].value) >= 0) {
+                listaDeSueldos.push(Number(listaDeSueldosListaDeNodos[i].value));
+            }     
+        }
+        if (listaDeSueldos.length !== listaDeSueldosListaDeNodos.length) { // Manda mensaje de alerta
+            alert('Te faltó ingresar algún sueldo o ingresaste números negativos; no serán tenidos en cuenta para los cálculos');
+        }         
+        // Expresa los resultados
+        if (listaDeSueldos.length !== 0) {
+            document.querySelector('#mayor-salario-anual').textContent = `El salario anual más alto es de ${obtenerElMayor(listaDeSueldos)} pesos.`;
+            document.querySelector('#menor-salario-anual').textContent = `El salario anual más bajo es de ${obtenerElMenor(listaDeSueldos)} pesos.`;
+            document.querySelector('#promedio-salario-anual').textContent = `El salario anual promedio es de ${obtenerElPromedio(listaDeSueldos)} pesos.`;
+            document.querySelector('#promedio-salario-mensual').textContent = `El salario mensual promedio es de ${obtenerElPromedio(listaDeSueldos)/12} pesos.`;
+        }        
+
         return false;
     }
     // Ejecuta las acciones sobre el boton de limpiar: elimina los inputs y los resultados calculados. Vuelve a activar el boton#ingresar
@@ -110,6 +188,11 @@ document.querySelector('#ingresar').onclick = () => {
         document.querySelector('#mayor-edad').textContent = "";
         document.querySelector('#menor-edad').textContent = "";
         document.querySelector('#promedio-edad').textContent = "";
+        document.querySelector('#mayor-salario-anual').textContent = "";
+        document.querySelector('#menor-salario-anual').textContent = "";
+        document.querySelector('#promedio-salario-anual').textContent = "";
+        document.querySelector('#promedio-salario-mensual').textContent = "";
+
         // Vuelve a activar el boton#ingresar
         document.querySelector('#ingresar').disabled = false;
         // Vuelve el input al estado original
